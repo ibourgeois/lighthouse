ea38c69f7309724abe1ac437da4182b4d0dc1396
Derek Bourgeois <derek@ibourgeois.com>
2017-11-10 21:59:22 -0500

    update home route

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
