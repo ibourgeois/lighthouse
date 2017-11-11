cc0a4ccd7af6d167099903f7c810bc8b84bdd29f
Derek Bourgeois <derek@ibourgeois.com>
2017-11-11 00:49:40 -0500

    update views

--- resources/views/layouts/partials/admin-nav.blade.php
+++ resources/views/layouts/partials/admin-nav.blade.php
@@ -1,6 +1,6 @@
 <div class="panel panel-default">
     <div class="list-group">
-        <a href="#" class="list-group-item">Dashboard</a>
+        <a href="{{ route('admin.index') }}" class="list-group-item">Dashboard</a>
         <a href="#" class="list-group-item">Users</a>
         <a href="#" class="list-group-item">Projects</a>
     </div>
